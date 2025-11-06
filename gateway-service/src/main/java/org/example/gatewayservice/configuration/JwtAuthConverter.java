7e9bc9bf16016ab1dbcf70068a17bbe5749b3c2b
HakimMohammed <mohamed.hakim.dev@gmail.com>
2025-11-06 11:18:48 +0100

    Keycloack configuration with ROLES

---
+++ gateway-service/src/main/java/org/example/gatewayservice/configuration/JwtAuthConverter.java
@@ -0,0 +1,48 @@
+package org.example.gatewayservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Configuration
+public class JwtAuthConverter implements Converter<Jwt, Mono<AbstractAuthenticationToken>> {
+
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+    @Override
+    public Mono<AbstractAuthenticationToken> convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                        jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                        extractResourceRoles(jwt).stream())
+                .collect(Collectors.toSet());
+
+        AbstractAuthenticationToken authToken =
+                new JwtAuthenticationToken(jwt, authorities, jwt.getClaimAsString("preferred_username"));
+
+        return Mono.just(authToken);
+    }
+
+    private Collection<GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+        if (realmAccess == null) {
+            return Set.of();
+        }
+
+        Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+}
--- gateway-service/src/main/java/org/example/gatewayservice/configuration/SecurityConfig.java
+++ gateway-service/src/main/java/org/example/gatewayservice/configuration/SecurityConfig.java
@@ -2,30 +2,31 @@ package org.example.gatewayservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    private JwtAuthConverter jwtAuthConverter;
+
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/*-service/api/**").permitAll()
+                        .pathMatchers("/*-service/api/**").hasAuthority("ADMIN")
                         .anyExchange().authenticated()
                 )
+                .oauth2ResourceServer(o2 -> o2.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)
+                ))
                 .build();
     }
 }
