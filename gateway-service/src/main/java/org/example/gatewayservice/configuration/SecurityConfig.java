b448965ca507daab73d0051b1c9dfd2bf738fa34
HakimMohammed <mohamed.hakim.dev@gmail.com>
2025-11-08 11:15:14 +0100

    implemented Swagger Docs

--- gateway-service/src/main/java/org/example/gatewayservice/configuration/SecurityConfig.java
+++ gateway-service/src/main/java/org/example/gatewayservice/configuration/SecurityConfig.java
@@ -2,9 +2,15 @@ package org.example.gatewayservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,14 +25,30 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/*-service/api/**").hasAuthority("ADMIN")
+                        .pathMatchers("/swagger-ui.html").permitAll()
+                        .pathMatchers("/webjars/swagger-ui/**").permitAll()
+                        .pathMatchers("/v3/api-docs/**").permitAll()
+                        .pathMatchers("/*-service/api/**").authenticated()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(o2 -> o2.jwt(
                         jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)
                 ))
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
